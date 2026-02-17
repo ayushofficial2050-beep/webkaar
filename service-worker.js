e155916e5797f40531669e8b59a2d17f96f1dec6
Ayush Tiwari <ayushofficial2050-beep@gmail.com>
2026-02-17 15:53:15 +0530

    New updates after restore

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,10 @@
 /**
  * WebKaar Ultimate Service Worker
  * Features: Instant Updates, Offline Support, Smart Caching
- * Version: v33 (Always change this number when you update code)
+ * Version: v35 (Always change this number when you update code)
  */
 
-const CACHE_NAME = 'webkaar-v33-live'; // ✅ Jab bhi code update karo, ye number badha dena (v8 -> v33)
+const CACHE_NAME = 'webkaar-v35-live'; // ✅ Jab bhi code update karo, ye number badha dena (v8 -> v35)
 
 // 1. FILES TO CACHE IMMEDIATELY (App Shell)
 // Sirf wo files jo website khulne ke liye zaroori hain
