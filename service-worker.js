c0e5b162c5c9a0f5eb86fcf450c2a8939193f4b4
Ayush <ayush@github.com>
2026-03-11 22:39:15 +0530

    Added more new changes

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,10 @@
 /**
  * WebKaar Ultimate Service Worker
  * Features: Instant Updates, Offline Support, Smart Caching
- * Version: v41 (Always change this number when you update code)
+ * Version: v42 (Always change this number when you update code)
  */
 
-const CACHE_NAME = 'webkaar-v41-live'; // ✅ Jab bhi code update karo, ye number badha dena (v8 -> v41)
+const CACHE_NAME = 'webkaar-v42-live'; // ✅ Jab bhi code update karo, ye number badha dena (v8 -> v42)
 
 // 1. FILES TO CACHE IMMEDIATELY (App Shell)
 // Sirf wo files jo website khulne ke liye zaroori hain
